0183794c614b335ba726f59c820752c58a664d53
sidereus <francesco.serafin.3@gmail.com>
2015-10-19 17:22:02 -0600

    Added the main class. RiverNe3 is going to be the main OMS class with the @execute method. At the moment is just a test class.

---
+++ src/com/wordpress/growworkinghard/riverNe3/RiverNe3.java
@@ -0,0 +1,71 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2015 AboutHydrology (Riccardo Rigon)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wordpress.growworkinghard.riverNe3;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import com.wordpress.growworkinghard.riverNe3.composite.Component;
+import com.wordpress.growworkinghard.riverNe3.dbfProcessing.DbfLinesProcessing;
+import com.wordpress.growworkinghard.riverNe3.dbfProcessing.DbfProcessing;
+import com.wordpress.growworkinghard.riverNe3.geometry.Geometry;
+
+public class RiverNe3 {
+ 
+    static DbfProcessing dfbp;
+    static ConcurrentHashMap<Integer, Geometry> test;
+    static TreeBuilding tb;
+    static ConcurrentHashMap<Integer, Component> binaryTree;
+
+    public static void main(String[] args) {
+    
+        String filePath = "/home/francesco/vcs/git/personal/RiverNe3/data/net.dbf";
+        String[] colNames = {"pfaf", "X_start", "Y_start", "X_end", "Y_end"};
+
+        dfbp = new DbfLinesProcessing();
+        test = dfbp.get(filePath, colNames);
+
+        tb = new TreeBuilding();
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        executor.execute(new MyRunnable());
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {}
+
+        binaryTree = tb.get();
+        System.out.println(binaryTree);
+
+    }
+
+    public static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            tb.getTree(test);
+        }
+
+    }
+
+}
