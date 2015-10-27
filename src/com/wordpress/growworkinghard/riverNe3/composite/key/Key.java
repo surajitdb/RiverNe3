0eac093466c26d8da89ce5a749de53db45394a64
sidereus <francesco.serafin.3@gmail.com>
2015-10-26 20:37:44 -0600

    Add comment to Key class

--- src/com/wordpress/growworkinghard/riverNe3/composite/key/Key.java
+++ src/com/wordpress/growworkinghard/riverNe3/composite/key/Key.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import net.jcip.annotations.Immutable;
 
 /**
+ * @brief http://commons.apache.org/proper/commons-lang/download_lang.cgi
  * @todo make this object immutable
  *
  * @author
